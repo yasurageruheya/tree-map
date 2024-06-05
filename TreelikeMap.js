ff920e1db5614b96aefa6ee1cacd3e2ac4fecff0
工藤潤 <j.kudo@aobiji.ac.jp>
2024-06-05 17:16:12 +0900

    jsdocを追加

--- TreelikeMap.js
+++ TreelikeMap.js
@@ -15,10 +15,39 @@ class TreelikeMap
 		this.parent = parent;
 	}
 
+	/**
+	 *
+	 * @param key {any}
+	 * @return {boolean}
+	 */
 	has(key) { return this.children.has(key); }
+
+	/**
+	 *
+	 * @param key {any}
+	 * @return {TreelikeMap}
+	 */
 	get(key) { return this.children.get(key); }
-	set(key, value) { this.children.set(key, new TreelikeMap(value, this)); }
-	remove(key) { this.children.delete(key); }
+
+	/**
+	 *
+	 * @param key {any}
+	 * @param value {any}
+	 * @return {Map<TreelikeMap>}
+	 */
+	set(key, value) { return this.children.set(key, new TreelikeMap(value, this)); }
+
+	/**
+	 *
+	 * @param key {any}
+	 * @return {boolean}
+	 */
+	remove(key) { return this.children.delete(key); }
+
+	/**
+	 *
+	 * @return {number}
+	 */
 	get size() { return this.children.size; }
 }
 
