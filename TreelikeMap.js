65a34cd39c0899f1be4abd9c561f659f1513970c
工藤潤 <j.kudo@aobiji.ac.jp>
2024-06-05 17:07:30 +0900

    tree-map パッケージは既に存在してたみたいなので、treelike-map にパッケージ名を変更したり

---
+++ TreelikeMap.js
@@ -0,0 +1,25 @@
+class TreelikeMap
+{
+	/**
+	 *
+	 * @param value {any}
+	 * @param [parent] {TreelikeMap}
+	 */
+	constructor(value, parent)
+	{
+		/** @type {any} */
+		this.value = value;
+		/** @type {Map<TreelikeMap>} */
+		this.children = new Map();
+		/** @type {TreelikeMap} */
+		this.parent = parent;
+	}
+
+	has(key) { return this.children.has(key); }
+	get(key) { return this.children.get(key); }
+	set(key, value) { this.children.set(key, new TreelikeMap(value, this)); }
+	remove(key) { this.children.delete(key); }
+	get size() { return this.children.size; }
+}
+
+module.exports = TreelikeMap;
